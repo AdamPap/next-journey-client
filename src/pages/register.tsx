41660b837a6bab266183c5f34cc222311f230d4f
Adam <adampapamast@hotmail.com>
2021-10-23 09:31:06 +0300

    ADD: register page and connect to backend

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -4,13 +4,36 @@ import Wrapper from "../components/Wrapper";
 import { InputField } from "../components/InputField";
 import { Box } from "@chakra-ui/layout";
 import { Button } from "@chakra-ui/button";
+import { useMutation } from "urql";
 
-function Register() {
+const REGISTER_MUT = `
+mutation Register($username: String!, $password: String!, $name:  String!, $email: String!) {
+  register(options: {username: $username, password: $password, name: $name, email: $email}) {
+    errors {
+      field,
+      message
+    }
+    user {
+      id,
+      username,
+      name,
+      email
+      }
+    
+  }
+}
+`;
+
+const Register = () => {
+  const [{}, register] = useMutation(REGISTER_MUT);
+
   return (
     <Wrapper variant="small">
       <Formik
-        initialValues={{ username: "", password: "" }}
-        onSubmit={(values) => console.log(values)}
+        initialValues={{ username: "", password: "", name: "", email: "" }}
+        onSubmit={async (values) => {
+          const res = await register(values);
+        }}
       >
         {({ isSubmitting }) => (
           <Form>
@@ -27,6 +50,22 @@ function Register() {
                 type="password"
               />
             </Box>
+            <Box mt={4}>
+              <InputField
+                name="name"
+                label="name"
+                placeholder="name"
+                type="name"
+              />
+            </Box>
+            <Box mt={4}>
+              <InputField
+                name="email"
+                label="email"
+                placeholder="email"
+                type="email"
+              />
+            </Box>
             <Button
               mt={4}
               isLoading={isSubmitting}
@@ -40,6 +79,6 @@ function Register() {
       </Formik>
     </Wrapper>
   );
-}
+};
 
 export default Register;
