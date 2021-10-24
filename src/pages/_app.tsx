759a06d727b0d6218807449abc0eaeb02a291a7d
Adam <adampapamast@hotmail.com>
2021-10-24 10:54:58 +0300

    ADD: cache fix to update query after register and login mutations fire

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,73 @@
 import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
-import { Provider, createClient } from "urql";
-
+import { Provider, createClient, dedupExchange, fetchExchange } from "urql";
+import { Cache, cacheExchange, QueryInput } from "@urql/exchange-graphcache";
 import theme from "../theme";
+import {
+  CurrentUserDocument,
+  CurrentUserQuery,
+  LoginMutation,
+  RegisterMutation,
+} from "../generated/graphql";
+
+function betterUpdateQuery<Result, Query>(
+  cache: Cache,
+  qi: QueryInput,
+  result: any,
+  fn: (r: Result, q: Query) => Query
+) {
+  return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
+}
 
 const client = createClient({
   url: "http://localhost:3001/graphql",
   fetchOptions: {
     credentials: "include",
   },
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      updates: {
+        Mutation: {
+          login: (_result, args, cache, info) => {
+            betterUpdateQuery<LoginMutation, CurrentUserQuery>(
+              cache,
+              { query: CurrentUserDocument },
+              _result,
+              (result, query) => {
+                if (result.login.errors) {
+                  return query;
+                } else {
+                  return {
+                    currentUser: result.login.user,
+                  };
+                }
+              }
+            );
+          },
+          register: (_result, args, cache, info) => {
+            betterUpdateQuery<RegisterMutation, CurrentUserQuery>(
+              cache,
+              { query: CurrentUserDocument },
+              _result,
+              (result, query) => {
+                if (result.register.errors) {
+                  return query;
+                } else {
+                  return {
+                    currentUser: result.register.user,
+                  };
+                }
+              }
+            );
+          },
+        },
+      },
+    }),
+    fetchExchange,
+  ],
 });
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: any) {
   return (
     <Provider value={client}>
       <ChakraProvider resetCSS theme={theme}>
