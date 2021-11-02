8878f16c764b75ad6137292972278ec7e94319cf
Adam <adampapamast@hotmail.com>
2021-11-02 22:27:04 +0200

    ADD: cookie in headers  when ssr

--- src/utils/createUrqlClient.ts
+++ src/utils/createUrqlClient.ts
@@ -16,6 +16,7 @@ import {
   VoteMutationVariables,
 } from "../generated/graphql";
 import { betterUpdateQuery } from "./betterUpdateQuery";
+import { isServer } from "./isServer";
 
 export type MergeMode = "before" | "after";
 
@@ -70,115 +71,124 @@ export const cursorPagination = (): Resolver<any, any, any> => {
   };
 };
 
-export const createUrqlClient = (ssrExchange: any) => ({
-  url: "http://localhost:3001/graphql",
-  fetchOptions: {
-    credentials: "include" as const,
-  },
-  exchanges: [
-    dedupExchange,
-    cacheExchange({
-      keys: {
-        PaginatedCampgrounds: () => null,
-      },
-      resolvers: {
-        Query: {
-          campgrounds: cursorPagination(),
+export const createUrqlClient = (ssrExchange: any, ctx: any) => {
+  let cookie = "";
+  if (isServer()) {
+    cookie = ctx.req.headers.cookie;
+  }
+
+  return {
+    url: "http://localhost:3001/graphql",
+    fetchOptions: {
+      credentials: "include" as const,
+      // add cookie in header when it's ssr
+      headers: cookie ? { cookie } : undefined,
+    },
+    exchanges: [
+      dedupExchange,
+      cacheExchange({
+        keys: {
+          PaginatedCampgrounds: () => null,
         },
-      },
-      updates: {
-        Mutation: {
-          vote: (_result, args, cache, info) => {
-            const { campgroundId, value } = args as VoteMutationVariables;
+        resolvers: {
+          Query: {
+            campgrounds: cursorPagination(),
+          },
+        },
+        updates: {
+          Mutation: {
+            vote: (_result, args, cache, info) => {
+              const { campgroundId, value } = args as VoteMutationVariables;
 
-            const data = cache.readFragment(
-              gql`
-                fragment _ on Campground {
-                  id
-                  points
-                  voteStatus
-                }
-              `,
-              { id: campgroundId }
-            );
-
-            console.log("DATA: ", data);
-
-            if (data) {
-              if (data.voteStatus === value) {
-                return;
-              }
-
-              const newPoints =
-                data.points + (!data.voteStatus ? 1 : 2) * value;
-
-              cache.writeFragment(
+              const data = cache.readFragment(
                 gql`
                   fragment _ on Campground {
+                    id
                     points
                     voteStatus
                   }
                 `,
-                { id: campgroundId, points: newPoints, voteStatus: value }
+                { id: campgroundId }
               );
-            }
-          },
-          createCampground: (_result, args, cache, info) => {
-            const allFields = cache.inspectFields("Query");
-            const fieldInfos = allFields.filter(
-              (info) => info.fieldName === "campgrounds"
-            );
 
-            fieldInfos.forEach((fi) => {
-              // NOTE: value of limit has to match pagination query
-              // on index page
-              cache.invalidate("Query", "campgrounds", fi.arguments || {});
-            });
-          },
-          logout: (_result, args, cache, info) => {
-            betterUpdateQuery<LogoutMutation, CurrentUserQuery>(
-              cache,
-              { query: CurrentUserDocument },
-              _result,
-              () => ({ currentUser: null })
-            );
-          },
-          login: (_result, args, cache, info) => {
-            betterUpdateQuery<LoginMutation, CurrentUserQuery>(
-              cache,
-              { query: CurrentUserDocument },
-              _result,
-              (result, query) => {
-                if (result.login.errors) {
-                  return query;
-                } else {
-                  return {
-                    currentUser: result.login.user,
-                  };
+              console.log("DATA: ", data);
+
+              if (data) {
+                if (data.voteStatus === value) {
+                  return;
                 }
+
+                const newPoints =
+                  data.points + (!data.voteStatus ? 1 : 2) * value;
+
+                cache.writeFragment(
+                  gql`
+                    fragment _ on Campground {
+                      points
+                      voteStatus
+                    }
+                  `,
+                  { id: campgroundId, points: newPoints, voteStatus: value }
+                );
               }
-            );
-          },
-          register: (_result, args, cache, info) => {
-            betterUpdateQuery<RegisterMutation, CurrentUserQuery>(
-              cache,
-              { query: CurrentUserDocument },
-              _result,
-              (result, query) => {
-                if (result.register.errors) {
-                  return query;
-                } else {
-                  return {
-                    currentUser: result.register.user,
-                  };
+            },
+            createCampground: (_result, args, cache, info) => {
+              const allFields = cache.inspectFields("Query");
+              const fieldInfos = allFields.filter(
+                (info) => info.fieldName === "campgrounds"
+              );
+
+              fieldInfos.forEach((fi) => {
+                // NOTE: value of limit has to match pagination query
+                // on index page
+                cache.invalidate("Query", "campgrounds", fi.arguments || {});
+              });
+            },
+            logout: (_result, args, cache, info) => {
+              betterUpdateQuery<LogoutMutation, CurrentUserQuery>(
+                cache,
+                { query: CurrentUserDocument },
+                _result,
+                () => ({ currentUser: null })
+              );
+            },
+            login: (_result, args, cache, info) => {
+              betterUpdateQuery<LoginMutation, CurrentUserQuery>(
+                cache,
+                { query: CurrentUserDocument },
+                _result,
+                (result, query) => {
+                  if (result.login.errors) {
+                    return query;
+                  } else {
+                    return {
+                      currentUser: result.login.user,
+                    };
+                  }
                 }
-              }
-            );
+              );
+            },
+            register: (_result, args, cache, info) => {
+              betterUpdateQuery<RegisterMutation, CurrentUserQuery>(
+                cache,
+                { query: CurrentUserDocument },
+                _result,
+                (result, query) => {
+                  if (result.register.errors) {
+                    return query;
+                  } else {
+                    return {
+                      currentUser: result.register.user,
+                    };
+                  }
+                }
+              );
+            },
           },
         },
-      },
-    }),
-    ssrExchange,
-    fetchExchange,
-  ],
-});
+      }),
+      ssrExchange,
+      fetchExchange,
+    ],
+  };
+};
