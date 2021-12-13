26ebeee92bdf7a1f13000f32ea6df150b8a849b9
Adam <adampapamast@hotmail.com>
2021-12-13 16:27:40 +0200

    add: full width option for wrapper

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,18 +1,21 @@
 import { Box } from "@chakra-ui/layout";
 import React from "react";
 
-export type WrapperVariant = "small" | "regular";
+export type WrapperVariant = "small" | "regular" | "full";
 
 interface WrapperProps {
   variant?: WrapperVariant;
 }
 
-const Wrapper: React.FC<WrapperProps> = ({ children, variant = "regular" }) => {
+const Wrapper: React.FC<WrapperProps> = ({ children, variant = "full" }) => {
   return (
     <Box
       py={8}
+      px={8}
       mx="auto"
-      maxW={variant === "regular" ? "900px" : "400px"}
+      maxW={
+        variant === "regular" ? "900px" : variant === "small" ? "400px" : "100%"
+      }
       w="100%"
     >
       {children}
