e97b750ef5bcf83fb792b2974d0d1ec8a2e5d289
Adam <adampapamast@hotmail.com>
2021-12-14 00:05:29 +0200

    rework: login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Form, Formik } from "formik";
 import { InputField } from "../components/InputField";
-import { Box, Flex, Link } from "@chakra-ui/layout";
+import { Box, Flex, Heading, Link, Text } from "@chakra-ui/layout";
 import { Button } from "@chakra-ui/button";
 import { useLoginMutation } from "../generated/graphql";
 import { toErrorMap } from "../utils/toErrorMap";
@@ -16,55 +16,81 @@ const Login = () => {
   const [{}, login] = useLoginMutation();
 
   return (
-    <Layout variant="small">
-      <Formik
-        initialValues={{ username: "", password: "" }}
-        onSubmit={async (values, { setErrors }) => {
-          const res = await login({ options: values });
-          if (res.data?.login.errors) {
-            setErrors(toErrorMap(res.data.login.errors));
-          } else if (res.data?.login.user) {
-            if (typeof router.query.next === "string") {
-              router.push(router.query.next);
-            } else {
-              router.push("/");
-            }
-          }
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form>
-            <InputField
-              name="username"
-              label="Username"
-              placeholder="Username"
-            />
-            <Box mt={4}>
-              <InputField
-                name="password"
-                label="Password"
-                placeholder="Password"
-                type="password"
-              />
-            </Box>
-            <Button
-              mt={5}
-              isLoading={isSubmitting}
-              colorScheme="teal"
-              type="submit"
+    <Layout>
+      <Flex height="100%" minHeight="90vh">
+        <Box
+          display={{ base: "none", lg: "block" }}
+          width="50%"
+          backgroundImage="url('https://res.cloudinary.com/dnrjcvoom/image/upload/v1639422996/next-journey/photo-1498889444388-e67ea62c464b_kfyz9i.jpg')"
+          backgroundPosition="center"
+          backgroundRepeat="no-repeat"
+          backgroundSize="cover"
+        ></Box>
+        <Flex
+          width={{ base: "100%", lg: "50%" }}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Box maxWidth="400px" width="100%" p={8}>
+            <Heading textAlign="center" pb={2}>
+              Sign In
+            </Heading>
+            <Text textAlign="center" pb={6}>
+              Welcome back!
+            </Text>
+            <Formik
+              initialValues={{ username: "", password: "" }}
+              onSubmit={async (values, { setErrors }) => {
+                const res = await login({ options: values });
+                if (res.data?.login.errors) {
+                  setErrors(toErrorMap(res.data.login.errors));
+                } else if (res.data?.login.user) {
+                  if (typeof router.query.next === "string") {
+                    router.push(router.query.next);
+                  } else {
+                    router.push("/");
+                  }
+                }
+              }}
             >
-              Login
-            </Button>
-            <Flex mt={3}>
-              <NextLink href="/forgot-password">
-                <Link ml="auto" color="teal">
-                  Forgot Password?
-                </Link>
-              </NextLink>
-            </Flex>
-          </Form>
-        )}
-      </Formik>
+              {({ isSubmitting }) => (
+                <Form>
+                  <InputField
+                    name="username"
+                    label="Username"
+                    placeholder="Username"
+                  />
+                  <Box mt={4}>
+                    <InputField
+                      name="password"
+                      label="Password"
+                      placeholder="Password"
+                      type="password"
+                    />
+                  </Box>
+                  <Button
+                    width="100%"
+                    pt={1}
+                    mt={8}
+                    isLoading={isSubmitting}
+                    colorScheme="teal"
+                    type="submit"
+                  >
+                    Login
+                  </Button>
+                  <Flex mt={5}>
+                    <NextLink href="/forgot-password">
+                      <Link ml="auto" color="teal">
+                        Forgot your Password?
+                      </Link>
+                    </NextLink>
+                  </Flex>
+                </Form>
+              )}
+            </Formik>
+          </Box>
+        </Flex>
+      </Flex>
     </Layout>
   );
 };
