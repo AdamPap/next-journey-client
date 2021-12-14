d3195e4ac507e4fc8941bf63f4969508cb946d26
Adam <adampapamast@hotmail.com>
2021-12-14 19:49:30 +0200

    rework: edit and delete buttons

--- src/components/EditDeleteCampgroundButtons.tsx
+++ src/components/EditDeleteCampgroundButtons.tsx
@@ -1,5 +1,5 @@
 import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
-import { Button, Flex, IconButton } from "@chakra-ui/react";
+import { Button, Flex, IconButton, Box } from "@chakra-ui/react";
 import NextLink from "next/link";
 import React from "react";
 import { useDeleteCampgroundMutation } from "../generated/graphql";
@@ -15,18 +15,35 @@ export const EditDeleteCampgroundButtons: React.FC<EditDeleteCampgroundButtonsPr
     return (
       <Flex justifyContent="end">
         <NextLink href={"/campground/edit/[id]"} as={`/campground/edit/${id}`}>
-          <Button colorScheme="teal" px={2}>
-            <EditIcon />
+          <Button
+            size="sm"
+            leftIcon={<EditIcon />}
+            colorScheme="teal"
+            px={2}
+            // pt={1}
+          >
+            <Box pt={1}>Edit</Box>
           </Button>
         </NextLink>
 
-        <IconButton
+        <Button
+          size="sm"
+          leftIcon={<DeleteIcon />}
+          colorScheme="red"
+          px={2}
+          // pt={1}
+          ml={2}
+          onClick={() => deleteCampground({ id })}
+        >
+          <Box pt={1}>Delete</Box>
+        </Button>
+        {/* <IconButton
           aria-label="Delete campground"
           icon={<DeleteIcon />}
           colorScheme="red"
           ml={2}
           onClick={() => deleteCampground({ id })}
-        />
+        /> */}
       </Flex>
     );
   };
