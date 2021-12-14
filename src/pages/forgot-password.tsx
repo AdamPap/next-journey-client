b217852e20b9842fc8351e73dcf9c3cb2f7f9f72
Adam <adampapamast@hotmail.com>
2021-12-14 16:05:31 +0200

    rework: forgot password styles

--- src/pages/forgot-password.tsx
+++ src/pages/forgot-password.tsx
@@ -1,52 +1,76 @@
-import { Box } from "@chakra-ui/layout";
+import { Box, Flex, Heading } from "@chakra-ui/layout";
 import { Button } from "@chakra-ui/react";
 import { Form, Formik } from "formik";
 import { withUrqlClient } from "next-urql";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { InputField } from "../components/InputField";
+import { Layout } from "../components/Layout";
 import Wrapper from "../components/Wrapper";
 import { useForgotPasswordMutation } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
+import { useToast } from "@chakra-ui/react";
 
 const ForgotPassword: React.FC<{}> = ({}) => {
   const [, forgotPassword] = useForgotPasswordMutation();
   const [isComplete, setIsComplete] = useState(false);
 
+  const toast = useToast();
+
+  useEffect(() => {
+    if (isComplete) {
+      toast({
+        title: "An email has been sent to you.",
+        description:
+          "If there is an account associated with this email, you will receive a link and instructions to change your password.",
+        status: "success",
+        duration: 9000,
+        isClosable: true,
+      });
+    }
+  }, [isComplete]);
+
   return (
-    <Wrapper variant="small">
-      <Formik
-        initialValues={{ email: "" }}
-        onSubmit={async (values, { setErrors }) => {
-          await forgotPassword(values);
-          setIsComplete(true);
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form>
-            <InputField
-              name="email"
-              label="Email"
-              placeholder="Email"
-              type="email"
-            />
-            <Button
-              mt={4}
-              isLoading={isSubmitting}
-              colorScheme="teal"
-              type="submit"
-            >
-              Send Email
-            </Button>
-            {isComplete ? (
-              <Box mt={3} color="gray.600">
-                If there is an account associated with this email, you will
-                receive a link to change your password.
-              </Box>
-            ) : null}
-          </Form>
-        )}
-      </Formik>
-    </Wrapper>
+    <Layout variant="small">
+      <Box mt={10}>
+        <Heading textAlign="center" pb={6}>
+          Password Reset
+        </Heading>
+        <Formik
+          initialValues={{ email: "" }}
+          onSubmit={async (values, { setErrors }) => {
+            await forgotPassword(values);
+            setIsComplete(true);
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <InputField
+                name="email"
+                label="Email"
+                placeholder="Email"
+                type="email"
+              />
+              <Button
+                mt={4}
+                width="100%"
+                pt={1}
+                isLoading={isSubmitting}
+                colorScheme="teal"
+                type="submit"
+              >
+                Send Email
+              </Button>
+              {/* {isComplete ? (
+                <Box mt={5} color="teal">
+                  If there is an account associated with this email, you will
+                  receive a link and instructions to change your password.
+                </Box>
+              ) : null} */}
+            </Form>
+          )}
+        </Formik>
+      </Box>
+    </Layout>
   );
 };
 
